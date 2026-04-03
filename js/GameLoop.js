3448089f1ad93f4e7bba119e380b964fa2cb4a0d
Noshawott <NameableSound@gmail.com>
2026-04-03 13:31:42 -0400

    sad

--- js/GameLoop.js
+++ js/GameLoop.js
@@ -1,5 +1,37 @@
-var canvas, context, player;
+var canvas, context, player, timer, interval = 1000/60;
+
 canvas = document.getElementById("canvas")
 context = canvas.getContext("2d")
+
+
 player = new Player()
-player.draw();
+player.vx = 30;
+player.vy = 30;
+
+timer = setInterval(animate, interval);
+
+function animate()
+{
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    player.move();
+    if(player.x > canvas.width + player.width/2 - 100)
+    {
+        player.vx *= -1;
+    }
+    if (player.x < 0 + player.width/2)
+    {
+        player.vx = 30;
+    }
+    if(player.y > canvas.height + player.height/2 - 100)
+    {
+        player.vy *= -1;
+    }
+    if (player.y < 0 + player.height/2)
+    {
+        player.vy = 30;
+    }
+
+    player.draw();
+}
+
+
--- js/Player.js
+++ js/Player.js
@@ -10,6 +10,10 @@ function Player()
     // set players color
     this.color = "#ff0000"
 
+    // set up player's velocity
+    this.vx = 0;
+    this.vy = 0;
+
     this.draw = function()
     {
         context.save();
@@ -17,5 +21,11 @@ function Player()
         context.translate(this.x, this.y);
         context.fillRect((-this.width/2), (-this.height/2), this.width, this.height);
         context.restore();
-    };
+    }
+
+    this.move = function()
+    {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
 }
